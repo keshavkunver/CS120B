4ddccb08bc746b24cd9a14689a23a88d2037adad
Keshav <kkunv001@ucr.edu>
2020-08-01 04:30:41 -0700

    Fixed part 1

--- source/main.c
+++ source/main.c
@@ -15,27 +15,16 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00;	PORTA = 0xFF;
-	DDRC = 0xFF;	PORTC = 0x00;
-
-	unsigned char cntavail = 0x00;
+	DDRB = 0xFF;	PORTB = 0x00;
 
     /* Insert your solution below */
     while (1) 
     {
-	    //don't change PINA value
-	    unsigned char temp = PINA;
-
-	    //check PA3-PA0 one by one
-	    for(int i = 0; i < 4; i++)
-	    {
-		//if bit != 1 space is available
-		if(!(temp & 0x01))
-			cntavail++;
-
-		//shift one after checking first bit
-		temp = temp >> 1;
-	    }
-	    PORTC = cntavail;
+	   //PA0 and !PA1
+	   if(PINA == 0x01)
+		   PORTB = 0x01;
+	   else
+		   PORTB = 0x00;
     }
     return 1;
 }
--- turnin/kkunv001@cs.ucr.edu_lab2_part1.c
+++ turnin/kkunv001@cs.ucr.edu_lab2_part1.c
@@ -1,7 +1,7 @@
 /*	Author: lab
  *  Partner(s) Name: Keshav Kunver
  *	Lab Section:
- *	Assignment: Lab #2  Exercise #1
+ *	Assignment: Lab #2  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,16 +14,17 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRB = 0xFF;
-	PORTB = 0x00;
+	DDRA = 0x00;	PORTA = 0xFF;
+	DDRB = 0xFF;	PORTB = 0x00;
+
     /* Insert your solution below */
     while (1) 
     {
-	if(PINA == 0x01)
-		PORTB = 0x01;
-	else
-		PORTB = 0x00;	
-
+	   //PA0 and !PA1
+	   if(PINA == 0x01)
+		   PORTB = 0x01;
+	   else
+		   PORTB = 0x00;
     }
     return 1;
 }
