f74cb733bf1abfe96db63af308e82bdff680ef3c
Keshav <kkunv001@ucr.edu>
2020-08-06 23:55:35 -0700

    Working on lab4ex1

--- Lab4_StateMachines/source/main.c
+++ Lab4_StateMachines/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: lab
  *  Partner(s) Name: Keshav Kunver
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #4  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,11 +12,65 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+enum Light_States { Start, ,  } state;
 
+//skeleton code from zyBooks
+void TickFct_Lights()
+{
+  switch(LA_State) {   // Transitions
+     case LA_SMStart:  // Initial transition
+        LA_State = LA_s0;
+        break;
+
+     case LA_s0:
+        if (!A0) {
+           LA_State = LA_s0;
+        }
+        else if (A0) {
+           LA_State = LA_s1;
+        }
+        break;
+
+     case LA_s1:
+        if (!A0) {
+           LA_State = LA_s0;
+        }
+        else if (A0) {
+           LA_State = LA_s1;
+        }
+        break;
+
+     default:
+        LA_State = LA_SMStart;
+        break;
+  } // Transitions
+
+  switch(LA_State) {   // State actions
+     case LA_s0:
+        break;
+
+     case LA_s1:
+        B0 = A1;
+        break;
+
+     default:
+        break;
+   } // State actions
+}
+
+int main(void) 
+{
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00;	PORTA = 0xFF;
+	DDRB = 0xFF;	PORTB = 0x00;
+
+	unsigned char button;	
+	unsigned char output = 0x00;
     /* Insert your solution below */
-    while (1) {
+    while (1) 
+    {
+	button = ~PINA;
+	output = 0x00;
 
     }
     return 1;
